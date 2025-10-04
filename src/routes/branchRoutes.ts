804aa9ac6543ecc226733e13d22b55156ce2ae35
Navraj Singh <navrajsingh@rrc.ca>
2025-10-04 03:02:08 -0500

    Updated the branchRoutes

--- src/routes/branchRoutes.ts
+++ src/routes/branchRoutes.ts
@@ -1,11 +1,25 @@
 import express, { Router  } from "express";
 import * as branchController from "../controllers/branchController";
+import { validateRequest } from "src/middleware/validate";
+import { branchSchemas } from "src/validations/branchValidation";
 
 const router: Router = express.Router();
 
 router.get("/", branchController.getAllBranches);
-router.post("/", branchController .createBranch);
-router.put("/:id",branchController .updateBranch);
-router.delete("/:id", branchController .deleteBranch);
+
+router.post(
+    "/",
+    validateRequest(branchSchemas.create),
+    branchController .createBranch);
+
+router.put(
+    "/:id",
+    validateRequest(branchSchemas.update),
+    branchController .updateBranch);
+
+router.delete(
+    "/:id", 
+    validateRequest(branchSchemas.delete),
+    branchController .deleteBranch);
 
 export default router;
