7abd358e9169edcdad627dd0eea908a5bfdfa222
Navraj Singh <navrajsingh@rrc.ca>
2025-10-05 15:22:14 -0500

    Updated the branchcontrollers

--- src/controllers/branchController.ts
+++ src/controllers/branchController.ts
@@ -14,9 +14,9 @@ export const getAllBranches = async (
 ): Promise<void> => {
     try {
         const branch: Branches[] = await branchServices.getAllBranches();
-        res.status(HTTP_STATUS.OK).json(
-            successResponse(branch, "Branches retrieved successfully")
-        );
+        res
+            .status(HTTP_STATUS.OK)
+            .json(successResponse(branch, "Branches retrieved successfully"));
     } catch (error: unknown) {
         next(error)
     }
@@ -31,26 +31,16 @@ export const createBranch = async (
     next: NextFunction
 ): Promise<void> => {
     try{
-        if (!req.body.name) {
-            res.status(HTTP_STATUS.BAD_REQUEST).json({
-                message: "branch name is required"
-            });
-        } else if (!req.body.address) {
-            res.status(HTTP_STATUS.BAD_REQUEST).json({
-                message: "branch address is required "
-            });
-        } else if (!req.body.phone) {
-            res.status(HTTP_STATUS.BAD_REQUEST).json({
-                message: "branch phone is required"
-            });
-        } else{
-            const {name, address, phone} = req.body;
+        const {name, address, phone} = req.body;
             
-            const newBranch: Branches = await branchServices.createBranch({name, address, phone});
-            res.status(HTTP_STATUS.CREATED).json(
-                successResponse(newBranch, "Branch created successfully")
-            );
-        }
+        const newBranch: Branches = await branchServices.createBranch({
+            name, 
+            address, 
+            phone});
+
+        res
+            .status(HTTP_STATUS.CREATED)
+            .json(successResponse(newBranch, "Branch created successfully"));
     } catch (error: unknown) {
         next(error)
     }
@@ -65,31 +55,16 @@ export const updateBranch = async (
     next: NextFunction
 ): Promise<void> => {
     try{
-        if (Number.isNaN(Number(req.params.id))) {
-            res.status(HTTP_STATUS.BAD_REQUEST).json({
-                message:"ID is not valid"
-            });
-        } else if (!req.body.name) {
-            res.status(HTTP_STATUS.BAD_REQUEST).json({
-                message:"Branch name is required"
-            });
-        } else if (!req.body.address) {
-            res.status(HTTP_STATUS.BAD_REQUEST).json({
-                message:"branch address is required"
-            });
-        } else if (!req.body.phone) {
-            res.status(HTTP_STATUS.BAD_REQUEST).json({
-                message:"Branch phone is required"
-            });
-        } else {
-            const id: number = Number(req.params.id)
-            const {name, address, phone} = req.body;
+        const id: number = Number(req.params.id)
+        const {name, address, phone} = req.body;
 
-            const updatedBranch: Branches = await branchServices.updateBranch(id,{name, address, phone});
-            res.status(HTTP_STATUS.OK).json(
-                successResponse(updateBranch, "Branch updated successfully")
-            );
-        }
+        const updatedBranch: Branches = await branchServices.updateBranch(id,{
+            name, 
+            address, 
+            phone});
+        res
+            .status(HTTP_STATUS.OK)
+            .json(successResponse(updateBranch, "Branch updated successfully") );
     } catch (error: unknown) {
         next(error)
     }
@@ -107,8 +82,9 @@ export const deleteBranch = async (
         const id: string = req.params.id;
 
         await branchServices.deleteBranch(Number(id));
-        res.status(HTTP_STATUS.OK).json(
-            successResponse(null, "Branch deleted successfully")
+        res
+            .status(HTTP_STATUS.OK)
+            .json(successResponse(null, "Branch deleted successfully")
         );
     } catch (error: unknown) {
         next(error);
